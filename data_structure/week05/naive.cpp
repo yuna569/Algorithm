6cef0a28be1d4d1cca22d458462dc5446f1b9e71
yuna <yjeong1217@gmail.com>
2025-08-03 20:40:27 +0900

    갓성열 교수님의 아이디어

--- data_structure/week05/naive.cpp
+++ data_structure/week05/naive.cpp
@@ -5,44 +5,29 @@ using namespace std;
 
 int main(void)
 {
-    string text = "abababcabcabcdabccbaabdabcabccabcd";
-    int num[34][7] = {0, };
+    string text = "abababcabcabcdabccbaabdabcabcdabcd";
+    int num[34][2] = {0, };
     string pattern = "abcabcd";
 
     bool flag = false;
     int j = 0;
+    int k = 0;
 
     for (int i=0; i<text.length(); i++) {
-        if (text[i] == pattern[i-j]) {
-            if (flag) { for (int k=0; k<7; k++) { if (num[i-1][k] != 0) num[i][k] = num[i-1][k] + 1;} }
-            else {
-                num[i][0] = 1;
-                flag = true;
-            }
-        }
-        else {
-            flag = false;
-            j = i;
-            if (text[i] == pattern[i-j]) {
-                if (flag) { for (int k=0; k<7; k++) { num[i][k] = num[i-1][k] + 1;} }
-                else {
-                num[i][0] = 1;
-                flag = true;
-                int l = 1;
-                for (int k=0; k<7; k++) {
-                    if (k == i-j) continue;
-                    if (pattern[k] == text[i]) {
-                        num[i][l] = k+1;
-                    }
+        for (int j=0; j<7; j++) {
+            if (text[i+j] == pattern[j]) {
+                if (num[i+j][0]!=0) {
+                    num[i+j][1] = j+1;
                 }
+                else num[i+j][0] = j+1;
             }
-            }
+            else break;
         }
     }
 
     for (int i=0; i<text.length(); i++) {
         cout << i << ": ";
-        for (int j=0; j<7; j++) {
+        for (int j=0; j<2; j++) {
             cout << num[i][j] << ", ";
         }
         cout << endl;
